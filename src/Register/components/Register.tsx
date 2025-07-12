076018095607d3dacd598dab618de543767d7d75
D3190 <aadarsha.love@gmail.com>
2025-07-12 16:37:17 +0545

    branch making

---
+++ src/Register/components/Register.tsx
@@ -0,0 +1,9 @@
+const Register=()=> {
+return (
+        <div className="min-h-screen  text-white px-60 py-10 bg-gray-700">
+
+            <h1 className="text-2xl ">Register</h1>
+        </div>
+)
+}
+export default Register
